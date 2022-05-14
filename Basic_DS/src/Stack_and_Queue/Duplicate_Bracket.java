a0c535760850c1fe2934b3fe5cd8e252391b9f11
Manish Mani Dinkar <moneyyiiss@gmail.com>
2022-05-14 18:42:43 +0530

    final of Duplicate Bracket

--- Basic_DS/src/Stack_and_Queue/Duplicate_Bracket.java
+++ Basic_DS/src/Stack_and_Queue/Duplicate_Bracket.java
@@ -1,7 +1,30 @@
 package Stack_and_Queue;
 
+import java.util.Scanner;
+import java.util.Stack;
+
 public class Duplicate_Bracket {
 	public static void main(String[] args) {
-		System.out.println();
+		Scanner scn = new Scanner(System.in);
+		String str = scn.nextLine();
+		
+		Stack<Character> st = new Stack<>();
+		for(int i=0; i<str.length(); i++) {
+			char ch = str.charAt(i);
+			if(ch == ')') {
+				if(st.peek() == '(') {
+					System.out.println(true);
+					return;
+				}else {
+					while(st.peek() != '(') {
+						st.pop();
+					}
+					st.pop();
+				}
+			}else {
+				st.push(ch);
+			}
+		}
+		System.out.println(false);
 	}
 }

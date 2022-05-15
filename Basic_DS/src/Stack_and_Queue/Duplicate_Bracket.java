3c08f6580d83766943653fce0d9f81849e5058f1
Manish Mani Dinkar <moneyyiiss@gmail.com>
2022-05-15 09:02:39 +0530

    Updated question of Duplicate Bracket

--- Basic_DS/src/Stack_and_Queue/Duplicate_Bracket.java
+++ Basic_DS/src/Stack_and_Queue/Duplicate_Bracket.java
@@ -1,3 +1,28 @@
+//Question
+//1. You are given a string exp representing an expression. 2. Assume that the expression is balanced i.e. the opening and closing brackets match with each other. 3. But, some of the pair of brackets maybe extra/needless. 4. You are required to print true if you detect extra brackets and false otherwise. e.g.' ((a + b) + (c + d)) -> false (a + b) + ((c + d)) -> true
+//
+//
+//Constraints
+//0 <= str.length <= 100
+//
+//
+//Format
+//Input
+//A string str
+//
+//
+//Output
+//true or false
+//
+//
+//Example
+//Sample Input
+//
+//(a + b) + ((c + d))
+//
+//Sample Output
+//true
+
 package Stack_and_Queue;
 
 import java.util.Scanner;
@@ -28,3 +53,5 @@ public class Duplicate_Bracket {
 		System.out.println(false);
 	}
 }
+
+

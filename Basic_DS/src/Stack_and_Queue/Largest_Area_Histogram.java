21e33fdc8a2c86c4bb531c5229718d0ff61b5e31
Manish Mani Dinkar <moneyyiiss@gmail.com>
2022-05-19 23:57:33 +0530

    updated

---
+++ Basic_DS/src/Stack_and_Queue/Largest_Area_Histogram.java
@@ -0,0 +1,102 @@
+//1. You are given a number n, representing the size of array a. 
+//2. You are given n numbers, representing the height of bars in a bar chart. 
+//3. You are required to find and print the area of largest rectangle in the histogram. 
+//e.g. for the array [6 2 5 4 5 1 6] -> 12
+//
+//
+//Constraints
+//0 <= n < 20 0 <= a[i] <= 10
+//
+//
+//Format
+//Input
+//Input is managed for you
+//
+//
+//Output
+//A number representing area of largest rectangle in histogram
+//
+//
+//Example
+//Sample Input
+//
+//7
+//6
+//2
+//5
+//4
+//5
+//1
+//6
+//
+//Sample Output
+//12
+
+
+package Stack_and_Queue;
+
+
+import java.io.*;
+import java.util.*;
+
+public class Largest_Area_Histogram{
+  
+
+public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int n = Integer.parseInt(br.readLine());
+    int[] arr = new int[n];
+    for(int i = 0; i < n; i++){
+       arr[i] = Integer.parseInt(br.readLine());
+    } 
+
+    // code
+    
+    int[] rb = new int[arr.length]; //nse index on the right
+    Stack<Integer> st = new Stack<>();
+    st.push(arr.length -1);
+    rb[arr.length-1] = arr.length;
+    
+    for(int i = arr.length -2; i>=0; i--) {
+    	while(st.size() > 0 && arr[i] < arr[st.peek()]) {
+    		st.pop();
+    	}
+    	if(st.size() == 0) {
+    		rb[i] = arr.length;
+    	}else {
+    		rb[i] = st.peek();
+    	}
+    	st.push(i);
+    }
+    
+    int[] lb = new int[arr.length]; //nse index on the left
+    st = new Stack<>();
+    st.push(0);
+    rb[0] = -1;
+    
+    for(int i = 1; i<=arr.length-1; i++) {
+    	while(st.size() > 0 && arr[i] < arr[st.peek()]) {
+    		st.pop();
+    	}
+    	if(st.size() == 0) {
+    		lb[i] = -1;
+    	}else {
+    		lb[i] = st.peek();
+    	}
+    	st.push(i);
+    	
+    }
+    
+    //area
+    int maxArea = 0;
+    for(int i=0; i<arr.length; i++) {
+    	int width = rb[i] - lb[i] - 1;
+    	int area = arr[i] * width;
+    	if(area > maxArea) {
+    		maxArea = area;
+    	}
+    }
+    System.out.println(maxArea);
+ }
+}

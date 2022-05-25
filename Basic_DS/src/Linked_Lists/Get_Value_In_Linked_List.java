5d8e6918bacf54cb06e6b7e42fc538ef03eac422
Manish Mani Dinkar <moneyyiiss@gmail.com>
2022-05-25 23:27:14 +0530

    updated

--- Basic_DS/src/Linked_Lists/Get_Value_In_Linked_List.java
+++ Basic_DS/src/Linked_Lists/Get_Value_In_Linked_List.java
@@ -55,15 +55,41 @@ public class Get_Value_In_Linked_List {
 
     public int getFirst(){
       // write your code here
+    	if(size == 0) {
+    		System.out.println("List is empty");
+    		return -1;
+    	}else {
+    		return head.data;
+    	}
    
     }
 
     public int getLast(){
       // write your code here
+    	if(size == 0) {
+    		System.out.println("List is empty");
+    		return -1;
+    	}else {
+    		return tail.data;
+    	}
+    	
     }
 
     public int getAt(int idx){
       // write your code here
+    	if(size == 0) {
+    		System.out.println("List is empty");
+    		return -1;
+    	}else if(idx < 0 || idx >= size) {
+    		System.out.println("Invalid arguments");
+    		return -1;
+    	}else {
+    		Node temp = head;
+    		for(int i=0; i<idx; i++) {
+    			temp = temp.next;
+    		}
+    		return temp.data;
+    	}
     }
   }
 
